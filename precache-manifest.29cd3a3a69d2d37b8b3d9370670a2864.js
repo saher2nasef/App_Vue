b00fc3785ff6708ba58a57e4fe8b4060a84ddc4a
saher_nasef <81627217+saher2nasef@users.noreply.github.com>
2022-02-11 19:50:47 +0200

    Add files via upload

---
+++ precache-manifest.29cd3a3a69d2d37b8b3d9370670a2864.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2b24afb2d9be2207117",
+    "url": "/css/app.6c4a1e0e.css"
+  },
+  {
+    "revision": "d4deb514757ee9ed14a0",
+    "url": "/css/chunk-vendors.54bcddb4.css"
+  },
+  {
+    "revision": "c72767964041959a628712dfe16db366",
+    "url": "/img/bg-pattern-footer-desktop.c7276796.png"
+  },
+  {
+    "revision": "17b6d689905926e6031fe88165f72cb6",
+    "url": "/img/bg-pattern-how-we-work-desktop.17b6d689.png"
+  },
+  {
+    "revision": "15df8e25e1b0b05b071ffc1486513da4",
+    "url": "/img/bg-pattern-how-we-work-mobile.15df8e25.png"
+  },
+  {
+    "revision": "8ac69ec7c30fa6465fa05426a4b235f4",
+    "url": "/img/image-intro-desktop.8ac69ec7.jpg"
+  },
+  {
+    "revision": "f700a415e89f97cf18c704f7f675cc4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2b24afb2d9be2207117",
+    "url": "/js/app.b4c4c09e.js"
+  },
+  {
+    "revision": "d4deb514757ee9ed14a0",
+    "url": "/js/chunk-vendors.bec2498c.js"
+  },
+  {
+    "revision": "8330a50a8cbc370f1ee7d5b8343280ef",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
